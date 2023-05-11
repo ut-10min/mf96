b2d481f6a6f502886fa49f484f85940b30ec1925
high-rubber <g-takashi120@g.ecc.u-tokyo.ac.jp>
2023-05-11 21:36:16 +0900

    Copy from kf68

--- js/main.js
+++ js/main.js
@@ -3,10 +3,4 @@ $(function() {
   Mustache.parse(template);
   var rendered = Mustache.render(template, {talks: data});
   $('.article-headline').html(rendered);
-
-  var hash_temp = location.hash;
-  if (hash_temp) {
-    location.replace('#');
-    location.replace(hash_temp);
-  }
 });

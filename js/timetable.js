528ac62640767558c82d55a60d9c5ad09a4ad526
high-rubber <g-takashi120@g.ecc.u-tokyo.ac.jp>
2023-05-12 01:17:13 +0900

    Update date

--- js/timetable.js
+++ js/timetable.js
@@ -3,13 +3,13 @@ function construstTimeTable(timeTable, talksData) {
     .filter(function (k) {return timeTable[k];})
     .sort()
     .map(function (time) {
-      console.log(time);
+      // console.log(time);
       var name = timeTable[time];
-      console.log(name);
+      // console.log(name);
       var index = 0;
 
       var talk = talksData.filter(function (t) { return t.name.indexOf(name) == 0; })[index];
-      console.log(talk)
+      // console.log(talk)
 
       if (name == "改行") {
         return { time: "\xa0", name: "\xa0", title: "", major: ""};

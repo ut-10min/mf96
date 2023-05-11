3a973301de9c5bd99529b2a5faa6f22c43b14808
high-rubber <g-takashi120@g.ecc.u-tokyo.ac.jp>
2023-05-12 00:54:55 +0900

    Update date

--- js/timetable.js
+++ js/timetable.js
@@ -13,8 +13,8 @@ function construstTimeTable(timeTable, talksData) {
       if (name == "改行") {
         return { time: "\xa0", name: "\xa0", title: "", major: ""};
       }
-      if (name == "休憩・座談会") {
-        return { time: time, name: name, title: "", major: ""};
+      else if (name == "休憩・座談会") {
+        return { time: time, name: "休憩・座談会", title: "", major: ""};
       }
       else {
         return { time: time, name: talk.name, title: talk.title, major: talk.affiliation };

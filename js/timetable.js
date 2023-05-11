7f6fd622fccfb823a213a33714456f4a69a82a3a
high-rubber <g-takashi120@g.ecc.u-tokyo.ac.jp>
2023-05-11 21:35:23 +0900

    Added string query

--- js/timetable.js
+++ js/timetable.js
@@ -1,43 +1,32 @@
 function construstTimeTable(timeTable, talksData) {
   return Object.keys(timeTable)
-    .filter(function (k) {return timeTable[k];})
-    .sort()
-    .map(function (time) {
-      // console.log(time);
-      var name = timeTable[time];
-      // console.log(name);
+               .filter(function (k) {return timeTable[k];})
+               .sort()
+               .map(function (time) {
+                 var name = timeTable[time];
+                 var index = 0;
 
-      if (name == "改行") {
-        return { time: "\xa0", name: "\xa0", title: "", major: ""};
-      }
-      if (name.indexOf("休憩・座談会") == 0) {
-        return { time: time, name: "", title: name, major: "" };
-      }
+                 if (name == "なおと") {
+                   name = "宇佐美尚人";
+                 } else if (name.indexOf("こすも") == 0) {
+                   index = parseInt(name.charAt(3)) - 1;
+                   name = "宇佐美こすも";
+                 }
 
-      var index = 0;
+                 var talk = talksData.filter(function (t) { return t.name.indexOf(name) == 0; })[index];
 
-      // if (name == "なおと") {
-      //   name = "宇佐美尚人";
-      // } else if (name.indexOf("こすも") == 0) {
-      //   index = parseInt(name.charAt(3)) - 1;
-      //   name = "宇佐美こすも";
-      // }
-      var talk = talksData.filter(function (t) { return t.name.indexOf(name) == 0; })[index];
-      return { time: time, name: talk.name, title: talk.title, major: talk.affiliation };
-    });
+                 return { time: time, name: talk.name, title: talk.title, major: talk.affiliation };
+               });
 }
 
 
 $(function () {
   var firstDayTable = construstTimeTable(day1, data);
   var secondDayTable = construstTimeTable(day2, data);
-  // var thirdDayTable = construstTimeTable(day3, data);
 
   var template = $('#template').html();
   Mustache.parse(template);
-  var renderedFirst = Mustache.render(template, {table: firstDayTable, header: "5/13 (土)"});
-  var renderedSecond = Mustache.render(template, {table: secondDayTable, header: "5/14 (日)"});
-  // var renderedThird = Mustache.render(template, {table: thirdDayTable, header: "5/15 (月)"});
-  // $('.article-headline').html(renderedFirst + "<br />" + renderedSecond + "<br />" + renderedThird);
-  $('.article-headline').html(renderedFirst + "<br />" + renderedSecond);
+  var renderedFirst = Mustache.render(template, {table: firstDayTable, header: "5/13(金)"});
+  var renderedSecond = Mustache.render(template, {table: secondDayTable, header: "5/14(土)"});
+  $('.article-headline').html(renderedFirst + "<br />" + renderedSecond + "<br />" + renderedThird);
 });
